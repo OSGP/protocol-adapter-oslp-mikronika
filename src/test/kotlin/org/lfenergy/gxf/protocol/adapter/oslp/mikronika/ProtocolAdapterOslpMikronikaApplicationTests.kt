3fceedfb4a08ed6876151fa03f3d158502c7f4f2
JKruisheer <kruisheer@outlook.com>
2025-09-25 07:54:39 +0200

    FDP-3254 team feedback
    Signed-off-by: JKruisheer <kruisheer@outlook.com>

---
+++ src/test/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/ProtocolAdapterOslpMikronikaApplicationTests.kt
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: Copyright Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.gxf.protocol.adapter.oslp.mikronika
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProtocolAdapterOslpMikronikaApplicationTests {
+    @Test
+    fun contextLoads() {
+        // Test startUp application
+    }
+}
