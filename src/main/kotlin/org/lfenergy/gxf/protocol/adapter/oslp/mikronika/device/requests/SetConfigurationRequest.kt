e1e69d89d81f410afd0ea86b21e32d313bc2e1ee
Sander van der Heijden <smvdheijden@users.noreply.github.com>
2026-01-30 13:42:39 +0100

    Feature/fdp 3670 set configuration update (#78)
    * FDP-3670 ~ Updates set configuration request
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    
    * FDP-3670 ~ Updates test
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    
    * FDP-3670 ~ Fixes Spotless issues
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    
    * FDP-3670 ~ Resolves sonar issue
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    
    * FDP-3670 ~ Adds additional assertions
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    
    * refactor(FDP-3670): Change the way the request is transformed internally
    
    Signed-off-by: Morten Minke <morten.minke@alliander.com>
    
    * chore(FDP-3670): Remove unnecessary apply
    
    Signed-off-by: Morten Minke <morten.minke@alliander.com>
    
    ---------
    
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>
    Signed-off-by: Morten Minke <morten.minke@alliander.com>
    Co-authored-by: Morten Minke <morten.minke@alliander.com>

--- src/main/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/device/requests/SetConfigurationRequest.kt
+++ src/main/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/device/requests/SetConfigurationRequest.kt
@@ -5,9 +5,18 @@ package org.lfenergy.gxf.protocol.adapter.oslp.mikronika.device.requests
 
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.domain.Device
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.domain.Organization
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.AstronomicalOffsetsConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.CommunicationConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.DaylightSavingsTimeConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.DeviceAddressConfiguration
 import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.LightType
 import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.LinkType
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.PlatformAddressConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.RelayConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.RelayLinkMatrix
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.RelayMap
 import org.opensmartgridplatform.oslp.Oslp
+import org.opensmartgridplatform.oslp.SetConfigurationRequestKt
 import org.opensmartgridplatform.oslp.indexAddressMap
 import org.opensmartgridplatform.oslp.message
 import org.opensmartgridplatform.oslp.relayConfiguration
@@ -24,62 +33,113 @@ class SetConfigurationRequest(
         device,
         organization,
     ) {
-    override fun toOslpMessage(): Oslp.Message =
-        message {
+    override fun toOslpMessage(): Oslp.Message {
+        val source = this.setConfigurationRequest.configuration
+
+        return message {
             setConfigurationRequest =
                 setConfigurationRequest {
-                    val setConfigRequest = this@SetConfigurationRequest.setConfigurationRequest.configuration
-
-                    relayLinking.addAll(
-                        setConfigRequest.relayConfiguration.relayLinking.relayLinkMatrixList.map {
-                            relayMatrix {
-                                masterRelayIndex = it.masterRelayIndex
-                                masterRelayOn = it.masterRelayOn
-                                indicesOfControlledRelaysOn = it.indicesOfControlledRelaysOn
-                                indicesOfControlledRelaysOff = it.indicesOfControlledRelaysOff
-                            }
-                        },
-                    )
-
-                    relayConfiguration =
-                        relayConfiguration {
-                            addressMap.addAll(
-                                setConfigRequest.relayConfiguration.relayMapping.relayMapList.map {
-                                    indexAddressMap {
-                                        index = it.index
-                                        address = it.address
-                                        relayType = it.relayType.toOslp()
-                                    }
-                                },
-                            )
-                        }
-
-                    relayRefreshing = setConfigRequest.relayConfiguration.relayRefreshingEnabled
-
-                    deviceFixIpValue = setConfigRequest.deviceAddressConfiguration.ipAddress
-                    netMask = setConfigRequest.deviceAddressConfiguration.netMask
-                    gateWay = setConfigRequest.deviceAddressConfiguration.gateway
-                    isDhcpEnabled = setConfigRequest.deviceAddressConfiguration.dhcpEnabled
-                    ospgIpAddress = setConfigRequest.platformAddressConfiguration.ipAddress
-                    osgpPortNumber = setConfigRequest.platformAddressConfiguration.portNumber
-                    preferredLinkType = setConfigRequest.communicationConfiguration.preferredLinkType.toOslp()
-                    communicationTimeout = setConfigRequest.communicationConfiguration.connectionTimeout
-                    communicationNumberOfRetries = setConfigRequest.communicationConfiguration.numberOfRetries
-                    communicationPauseTimeBetweenConnectionTrials =
-                        setConfigRequest.communicationConfiguration.delayBetweenConnectionAttempts
-                    isAutomaticSummerTimingEnabled =
-                        setConfigRequest.daylightSavingsTimeConfiguration.automaticSummerTimingEnabled
-                    summerTimeDetails = setConfigRequest.daylightSavingsTimeConfiguration.summerTimeDetails
-                    winterTimeDetails = setConfigRequest.daylightSavingsTimeConfiguration.winterTimeDetails
-                    astroGateSunRiseOffset = setConfigRequest.astronomicalOffsetsConfiguration.sunriseOffset
-                    astroGateSunSetOffset = setConfigRequest.astronomicalOffsetsConfiguration.sunsetOffset
-
-                    lightType = setConfigRequest.lightType.toOslp()
-                    isTestButtonEnabled = setConfigRequest.testButtonEnabled
-                    timeSyncFrequency = setConfigRequest.timeSyncFrequency
-                    switchingDelay.addAll(setConfigRequest.switchingDelayList)
+                    if (source.hasAstronomicalOffsetsConfiguration()) {
+                        add(source.astronomicalOffsetsConfiguration)
+                    }
+                    if (source.hasCommunicationConfiguration()) {
+                        add(source.communicationConfiguration)
+                    }
+                    if (source.hasDaylightSavingsTimeConfiguration()) {
+                        add(source.daylightSavingsTimeConfiguration)
+                    }
+                    if (source.hasDeviceAddressConfiguration()) {
+                        add(source.deviceAddressConfiguration)
+                    }
+                    if (source.hasPlatformAddressConfiguration()) {
+                        add(source.platformAddressConfiguration)
+                    }
+                    if (source.hasRelayConfiguration()) {
+                        add(source.relayConfiguration)
+                    }
+                    if (source.hasLightType()) lightType = source.lightType.toOslp()
+                    if (source.hasTestButtonEnabled()) isTestButtonEnabled = source.testButtonEnabled
+                    if (source.hasTimeSyncFrequency()) timeSyncFrequency = source.timeSyncFrequency
+                    if (source.switchingDelayCount > 0) switchingDelay.addAll(source.switchingDelayList)
                 }
         }
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: AstronomicalOffsetsConfiguration) {
+        if (source.hasSunsetOffset()) astroGateSunSetOffset = source.sunsetOffset
+        if (source.hasSunriseOffset()) astroGateSunRiseOffset = source.sunriseOffset
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: CommunicationConfiguration) {
+        if (source.hasPreferredLinkType()) preferredLinkType = source.preferredLinkType.toOslp()
+        if (source.hasConnectionTimeout()) communicationTimeout = source.connectionTimeout
+        if (source.hasNumberOfRetries()) communicationNumberOfRetries = source.numberOfRetries
+        if (source.hasDelayBetweenConnectionAttempts()) {
+            communicationPauseTimeBetweenConnectionTrials = source.delayBetweenConnectionAttempts
+        }
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: DaylightSavingsTimeConfiguration) {
+        if (source.hasAutomaticSummerTimingEnabled()) {
+            isAutomaticSummerTimingEnabled =
+                source.automaticSummerTimingEnabled
+        }
+        if (source.hasSummerTimeDetails()) summerTimeDetails = source.summerTimeDetails
+        if (source.hasWinterTimeDetails()) winterTimeDetails = source.winterTimeDetails
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: DeviceAddressConfiguration) {
+        if (source.hasIpAddress()) deviceFixIpValue = source.ipAddress
+        if (source.hasNetMask()) netMask = source.netMask
+        if (source.hasGateway()) gateWay = source.gateway
+        if (source.hasDhcpEnabled()) isDhcpEnabled = source.dhcpEnabled
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: PlatformAddressConfiguration) {
+        if (source.hasIpAddress()) ospgIpAddress = source.ipAddress
+        if (source.hasPortNumber()) osgpPortNumber = source.portNumber
+    }
+
+    private fun SetConfigurationRequestKt.Dsl.add(source: RelayConfiguration) {
+        if (source.hasRelayLinking()) {
+            relayLinking.addAll(source.relayLinking.relayLinkMatrixList.map { it.toOslp() })
+        }
+        if (source.hasRelayMapping()) {
+            relayConfiguration =
+                relayConfiguration {
+                    addressMap.addAll(
+                        source.relayMapping.relayMapList.map { it.toOslp() },
+                    )
+                }
+        }
+
+        if (source.hasRelayRefreshingEnabled()) relayRefreshing = source.relayRefreshingEnabled
+    }
+
+    private fun RelayLinkMatrix.toOslp(): Oslp.RelayMatrix {
+        val internal = this
+        return relayMatrix {
+            if (internal.hasMasterRelayIndex()) masterRelayIndex = internal.masterRelayIndex
+            if (internal.hasMasterRelayOn()) masterRelayOn = internal.masterRelayOn
+            if (internal.hasIndicesOfControlledRelaysOn()) {
+                indicesOfControlledRelaysOn =
+                    internal.indicesOfControlledRelaysOn
+            }
+            if (internal.hasIndicesOfControlledRelaysOff()) {
+                indicesOfControlledRelaysOff =
+                    internal.indicesOfControlledRelaysOff
+            }
+        }
+    }
+
+    private fun RelayMap.toOslp(): Oslp.IndexAddressMap {
+        val internal = this
+        return indexAddressMap {
+            if (internal.hasIndex()) index = internal.index
+            if (internal.hasAddress()) address = internal.address
+            if (internal.hasRelayType()) relayType = internal.relayType.toOslp()
+        }
+    }
 
     private fun LinkType.toOslp(): Oslp.LinkType =
         when (this) {
--- src/test/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/device/requests/SetConfigurationRequestTest.kt
+++ src/test/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/device/requests/SetConfigurationRequestTest.kt
@@ -3,14 +3,30 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.gxf.protocol.adapter.oslp.mikronika.device.requests
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import com.google.protobuf.ByteString
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.TestObjects.DEVICE_IDENTIFICATION
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.TestObjects.NETWORK_ADDRESS
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.mapper.ORGANIZATION_IDENTIFICATION
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.domain.Device
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.domain.Organization
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.LightType
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.LinkType
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.RelayType
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.astronomicalOffsetsConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.communicationConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.configuration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.daylightSavingsTimeConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.deviceAddressConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.platformAddressConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.relayConfiguration
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.relayLinkMatrix
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.relayLinking
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.relayMap
+import org.lfenergy.gxf.publiclighting.contracts.internal.configuration.relayMapping
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.setConfigurationRequest
+import org.opensmartgridplatform.oslp.Oslp
 
 class SetConfigurationRequestTest {
     @Test
@@ -19,11 +35,200 @@ class SetConfigurationRequestTest {
             SetConfigurationRequest(
                 Device(DEVICE_IDENTIFICATION, NETWORK_ADDRESS),
                 Organization(ORGANIZATION_IDENTIFICATION),
-                setConfigurationRequest = setConfigurationRequest { },
+                setConfigurationRequest =
+                    setConfigurationRequest {
+                        configuration =
+                            configuration {
+                                astronomicalOffsetsConfiguration =
+                                    astronomicalOffsetsConfiguration {
+                                        sunsetOffset = SUNSET_OFFSET
+                                        sunriseOffset = SUNRISE_OFFSET
+                                    }
+                                communicationConfiguration =
+                                    communicationConfiguration {
+                                        delayBetweenConnectionAttempts = DELAY_BETWEEN_CONNECTION_ATTEMPTS
+                                        numberOfRetries = NUMBER_OF_RETRIES
+                                        connectionTimeout = CONNECTION_TIMEOUT
+                                        preferredLinkType = LinkType.valueOf(PREFERRED_LINK_TYPE)
+                                    }
+                                daylightSavingsTimeConfiguration =
+                                    daylightSavingsTimeConfiguration {
+                                        automaticSummerTimingEnabled = true
+                                        summerTimeDetails = BEGIN_OF_DAYLIGHT_SAVINGS_TIME
+                                        winterTimeDetails = END_OF_DAYLIGHT_SAVINGS_TIME
+                                    }
+                                deviceAddressConfiguration =
+                                    deviceAddressConfiguration {
+                                        ipAddress = DEVICE_IP_ADDRESS
+                                        netMask = DEVICE_NET_MASK
+                                        gateway = DEVICE_GATEWAY
+                                        dhcpEnabled = true
+                                    }
+                                platformAddressConfiguration =
+                                    platformAddressConfiguration {
+                                        ipAddress = PLATFORM_IP_ADDRESS
+                                        portNumber = PLATFORM_PORT_NUMBER
+                                    }
+                                relayConfiguration =
+                                    relayConfiguration {
+                                        relayLinking = relayLinking { relayLinkMatrix.addAll(RELAY_LINKS) }
+                                        relayMapping = relayMapping { relayMap.addAll(RELAY_MAPS) }
+                                        relayRefreshingEnabled = true
+                                    }
+                                lightType = LightType.valueOf(LIGHT_TYPE)
+                                testButtonEnabled = true
+                                timeSyncFrequency = TIME_SYNC_FREQUENCY
+                                switchingDelay.addAll(SWITCHING_DELAYS)
+                            }
+                    },
             )
 
         val result = subject.toOslpMessage()
 
-        assertTrue(result.hasSetConfigurationRequest())
+        assertThat(result.hasSetConfigurationRequest()).isTrue
+        with(result.setConfigurationRequest) {
+            assertThat(astroGateSunSetOffset).isEqualTo(SUNSET_OFFSET)
+            assertThat(astroGateSunRiseOffset).isEqualTo(SUNRISE_OFFSET)
+
+            assertThat(communicationPauseTimeBetweenConnectionTrials).isEqualTo(DELAY_BETWEEN_CONNECTION_ATTEMPTS)
+            assertThat(communicationTimeout).isEqualTo(CONNECTION_TIMEOUT)
+            assertThat(communicationNumberOfRetries).isEqualTo(NUMBER_OF_RETRIES)
+            assertThat(preferredLinkType).isEqualTo(Oslp.LinkType.valueOf(PREFERRED_LINK_TYPE))
+
+            assertThat(isAutomaticSummerTimingEnabled).isTrue
+            assertThat(summerTimeDetails).isEqualTo(BEGIN_OF_DAYLIGHT_SAVINGS_TIME)
+            assertThat(winterTimeDetails).isEqualTo(END_OF_DAYLIGHT_SAVINGS_TIME)
+
+            assertThat(deviceFixIpValue).isEqualTo(DEVICE_IP_ADDRESS)
+            assertThat(netMask).isEqualTo(DEVICE_NET_MASK)
+            assertThat(gateWay).isEqualTo(DEVICE_GATEWAY)
+            assertThat(isDhcpEnabled).isTrue
+
+            assertThat(ospgIpAddress).isEqualTo(PLATFORM_IP_ADDRESS)
+            assertThat(osgpPortNumber).isEqualTo(PLATFORM_PORT_NUMBER)
+
+            assertThat(relayConfiguration.addressMapCount).isEqualTo(RELAY_MAPS.size)
+            assertThat(relayLinkingCount).isEqualTo(RELAY_LINKS.size)
+            assertThat(relayRefreshing).isTrue
+
+            assertThat(lightType).isEqualTo(Oslp.LightType.valueOf(LIGHT_TYPE))
+            assertThat(isTestButtonEnabled).isTrue
+            assertThat(timeSyncFrequency).isEqualTo(TIME_SYNC_FREQUENCY)
+            assertThat(switchingDelayCount).isEqualTo(SWITCHING_DELAYS.size)
+        }
+    }
+
+    @Test
+    fun `should map correctly when having only astronomical offsets`() {
+        val subject =
+            SetConfigurationRequest(
+                Device(DEVICE_IDENTIFICATION, NETWORK_ADDRESS),
+                Organization(ORGANIZATION_IDENTIFICATION),
+                setConfigurationRequest =
+                    setConfigurationRequest {
+                        configuration =
+                            configuration {
+                                astronomicalOffsetsConfiguration =
+                                    astronomicalOffsetsConfiguration {
+                                        sunsetOffset = SUNSET_OFFSET
+                                        sunriseOffset = SUNRISE_OFFSET
+                                    }
+                            }
+                    },
+            )
+
+        val result = subject.toOslpMessage()
+
+        assertThat(result.hasSetConfigurationRequest()).isTrue
+        with(result.setConfigurationRequest) {
+            assertThat(hasAstroGateSunSetOffset()).isTrue
+            assertThat(astroGateSunSetOffset).isEqualTo(SUNSET_OFFSET)
+
+            assertThat(hasAstroGateSunRiseOffset()).isTrue
+            assertThat(astroGateSunRiseOffset).isEqualTo(SUNRISE_OFFSET)
+
+            assertThat(hasCommunicationTimeout()).isFalse
+            assertThat(hasCommunicationPauseTimeBetweenConnectionTrials()).isFalse
+            assertThat(hasCommunicationNumberOfRetries()).isFalse
+            assertThat(hasPreferredLinkType()).isFalse
+
+            assertThat(hasIsAutomaticSummerTimingEnabled()).isFalse
+            assertThat(hasSummerTimeDetails()).isFalse
+            assertThat(hasWinterTimeDetails()).isFalse
+
+            assertThat(hasDeviceFixIpValue()).isFalse
+            assertThat(hasNetMask()).isFalse
+            assertThat(hasGateWay()).isFalse
+            assertThat(hasIsDhcpEnabled()).isFalse
+
+            assertThat(hasOspgIpAddress()).isFalse
+            assertThat(hasOsgpPortNumber()).isFalse
+
+            assertThat(hasRelayConfiguration()).isFalse
+            assertThat(relayLinkingCount).isEqualTo(0)
+            assertThat(hasRelayRefreshing()).isFalse
+
+            assertThat(hasLightType()).isFalse
+            assertThat(hasIsTestButtonEnabled()).isFalse
+            assertThat(hasTimeSyncFrequency()).isFalse
+            assertThat(switchingDelayCount).isEqualTo(0)
+        }
+    }
+
+    companion object {
+        const val SUNSET_OFFSET = -15
+        const val SUNRISE_OFFSET = 15
+
+        const val BEGIN_OF_DAYLIGHT_SAVINGS_TIME = "0360100" // Last sunday of March at 01:00 UTC
+        const val END_OF_DAYLIGHT_SAVINGS_TIME = "1060100" // Last sunday of October at 01:00 UTC
+
+        const val DELAY_BETWEEN_CONNECTION_ATTEMPTS = 1000
+        const val NUMBER_OF_RETRIES = 3
+        const val CONNECTION_TIMEOUT = 2000
+        const val PREFERRED_LINK_TYPE = "ETHERNET"
+
+        val DEVICE_IP_ADDRESS: ByteString = ByteString.copyFrom(byteArrayOf(127, 0, 0, 2))
+        val DEVICE_NET_MASK: ByteString = ByteString.copyFrom(byteArrayOf(255.toByte(), 255.toByte(), 255.toByte(), 0))
+        val DEVICE_GATEWAY: ByteString = ByteString.copyFrom(byteArrayOf(127, 0, 0, 1))
+
+        val PLATFORM_IP_ADDRESS: ByteString = ByteString.copyFrom(byteArrayOf(128.toByte(), 0, 0, 1))
+        const val PLATFORM_PORT_NUMBER = 22125
+
+        val RELAY_LINKS =
+            listOf(
+                relayLinkMatrix {
+                    masterRelayIndex = ByteString.copyFrom(byteArrayOf(1))
+                    masterRelayOn = true
+                    indicesOfControlledRelaysOn = ByteString.copyFrom(byteArrayOf(2, 3))
+                },
+                relayLinkMatrix {
+                    masterRelayIndex = ByteString.copyFrom(byteArrayOf(1))
+                    masterRelayOn = false
+                    indicesOfControlledRelaysOff = ByteString.copyFrom(byteArrayOf(2, 3))
+                },
+            )
+
+        val RELAY_MAPS =
+            listOf(
+                relayMap {
+                    index = ByteString.copyFrom(byteArrayOf(2))
+                    address = ByteString.copyFrom(byteArrayOf(1))
+                    relayType = RelayType.LIGHT
+                },
+                relayMap {
+                    index = ByteString.copyFrom(byteArrayOf(3))
+                    address = ByteString.copyFrom(byteArrayOf(2))
+                    relayType = RelayType.LIGHT
+                },
+                relayMap {
+                    index = ByteString.copyFrom(byteArrayOf(4))
+                    address = ByteString.copyFrom(byteArrayOf(3))
+                    relayType = RelayType.LIGHT
+                },
+            )
+
+        const val LIGHT_TYPE = "RELAY"
+        const val TIME_SYNC_FREQUENCY = 60
+        val SWITCHING_DELAYS = listOf(100, 200, 300, 400)
     }
 }
