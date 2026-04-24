bc220363b8785f56f42c4febd4b54aa396373878
Jessie Krul <jessie.krul@alliander.com>
2026-04-24 10:31:49 +0200

    Feature/fdp 3902 shipment files (#101)
    * feat(FDP-3635): Update key request impl
    
    Signed-off-by: Jessie Krul <jessie.krul@alliander.com>
    Signed-off-by: Jessie <jessie.krul@alliander.com>
    
    * feat(FDP-3635): fix integration test
    
    Signed-off-by: Jessie Krul <jessie.krul@alliander.com>
    Signed-off-by: Jessie <jessie.krul@alliander.com>
    
    * feat(FDP-3635): fix incorrect loglines
    
    Signed-off-by: Jessie Krul <jessie.krul@alliander.com>
    Signed-off-by: Jessie <jessie.krul@alliander.com>
    
    * feat(FDP-3635): failure flow
    
    Signed-off-by: Jessie <jessie.krul@alliander.com>
    
    * feat(FDP-3635): copilot
    
    Signed-off-by: Jessie <jessie.krul@alliander.com>
    
    ---------
    
    Signed-off-by: Jessie Krul <jessie.krul@alliander.com>
    Signed-off-by: Jessie <jessie.krul@alliander.com>

--- src/main/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/command/service/DeviceRequestServiceFactory.kt
+++ src/main/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/command/service/DeviceRequestServiceFactory.kt
@@ -7,9 +7,11 @@ import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request.
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request.RequestService
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request.SetLightRequestService
 import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request.SetScheduleRequestService
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request.UpdateKeyRequestService
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.DeviceRequestMessage
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.RequestType.SET_LIGHT_REQUEST
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.RequestType.SET_SCHEDULE_REQUEST
+import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.RequestType.UPDATE_KEY_REQUEST
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,11 +19,13 @@ class DeviceRequestServiceFactory(
     private val genericDeviceRequestService: GenericDeviceRequestService,
     private val setLightRequestService: SetLightRequestService,
     private val setScheduleRequestService: SetScheduleRequestService,
+    private val updateKeyRequestService: UpdateKeyRequestService,
 ) {
     fun getDeviceRequestServiceFor(requestMessage: DeviceRequestMessage): RequestService =
         when (requestMessage.header.requestType) {
             SET_LIGHT_REQUEST -> setLightRequestService
             SET_SCHEDULE_REQUEST -> setScheduleRequestService
+            UPDATE_KEY_REQUEST -> updateKeyRequestService
             else -> genericDeviceRequestService
         }
 }
---
+++ src/main/kotlin/org/lfenergy/gxf/protocol/adapter/oslp/mikronika/command/service/request/UpdateKeyRequestService.kt
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: Copyright Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.service.request
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.persistence.EntityNotFoundException
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.sender.DeviceResponseSender
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.command.util.HeaderUtil.buildResponseHeader
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.device.communication.service.DeviceClientService
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.device.communication.service.MikronikaDeviceService
+import org.lfenergy.gxf.protocol.adapter.oslp.mikronika.device.database.adapter.MikronikaDevice
+import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.DeviceRequestMessage
+import org.lfenergy.gxf.publiclighting.contracts.internal.device_responses.Result
+import org.lfenergy.gxf.publiclighting.contracts.internal.device_responses.deviceResponseMessage
+import org.springframework.stereotype.Service
+
+@Service
+class UpdateKeyRequestService(
+    deviceClientService: DeviceClientService,
+    private val mikronikaDeviceService: MikronikaDeviceService,
+    private val deviceResponseSender: DeviceResponseSender,
+) : RequestService(deviceClientService) {
+    private val logger = KotlinLogging.logger {}
+
+    override fun handleRequestMessage(requestMessage: DeviceRequestMessage) {
+        val deviceIdentification = requestMessage.header.deviceIdentification
+
+        try {
+            val mikronikaDevice =
+                try {
+                    mikronikaDeviceService.findByDeviceIdentification(deviceIdentification)
+                } catch (_: EntityNotFoundException) {
+                    MikronikaDevice(deviceIdentification = deviceIdentification)
+                }
+
+            mikronikaDevice.publicKey = requestMessage.updateKeyRequest.publicKey
+
+            mikronikaDeviceService.saveDevice(mikronikaDevice)
+
+            val responseMessage =
+                deviceResponseMessage {
+                    header = buildResponseHeader(requestMessage.header)
+                    result = Result.OK
+                }
+
+            deviceResponseSender.send(responseMessage)
+        } catch (e: Exception) {
+            logger.error(e) { "Exception occurred while updating key" }
+
+            deviceResponseSender.send(createErrorMessage(requestMessage.header, e))
+        }
+    }
+}
